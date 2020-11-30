f5ab3a078ccd0e95ef18509f842024e1cbb4633a
raulcg99 <59688527+raulcg99@users.noreply.github.com>
2020-11-30 14:08:40 +0100

    Actualizacion de la base de datos

--- conexionPrueba.php
+++ conexionPrueba.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once ('database.php');
+require_once ('database/database.php');
 
 try {
 	$db = DB::getInstance();
--- database/bd.sql
+++ database/bd.sql
@@ -6,6 +6,7 @@ id int auto_increment primary key,
 nombre varchar(50),
 pass varchar(50),
 telefono varchar(15),
+email varchar(30),
 direccion varchar(30));
 #Un insert para hacer pruebas
 insert into usuario (nombre,pass,telefono,direccion) values ('raul','1234','987654321','C/wkdw Nº12');
