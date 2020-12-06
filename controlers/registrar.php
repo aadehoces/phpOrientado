696fae07a0034e2d1d070e8d2c176e83ee64ed68
aadehoces <59688573+aadehoces@users.noreply.github.com>
2020-12-06 18:04:13 +0100

    mvc

---
+++ controlers/registrar.php
@@ -0,0 +1,25 @@
+<?php
+require_once '../administracion/botones.php';
+require_once '../administracion/validacion.php';
+	if ($_POST) {
+		if (isset($_POST['registro'])) {
+			$validar=new validar();
+			$mensaje=$validar->val_nombre($_POST['nombre']);
+			if ($mensaje=="true") {
+				$mensaje=$validar->val_apellidos($_POST['apellidos']);
+				if ($mensaje=="true") {
+					$mensaje=$validar->val_email($_POST['email']);
+					if ($mensaje=="true") {
+						$mensaje=$validar->val_direccion($_POST['direccion']);
+						if ($mensaje=="true") {
+							$mensaje=$validar->val_contraseña($_POST['contraseña']);
+							if ($mensaje=="true") {
+								//registrar
+							}
+						}
+					}
+				}		
+			}
+		}
+	}
+?>
