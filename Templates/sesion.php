04d965a906fd65d5a2fdb89d5872fbf527b4ce4c
aadehoces <59688573+aadehoces@users.noreply.github.com>
2020-12-06 19:56:57 +0100

    login

--- Templates/sesion.php
+++ Templates/sesion.php
@@ -1,20 +1,5 @@
 <?php
-require_once '../administracion/botones.php';
-require_once '../administracion/validacion.php';
-	if ($_POST) {
-		if (isset($_POST['login'])) {
-			$validar=new validar();
-			$mensaje=$validar->val_email($_POST['email']);
-			if ($mensaje=="true") {
-				$mensaje=$validar->val_contraseña($_POST['contraseña']);
-				if ($mensaje=="true") {
-					//$botones=new botones();
-					//$botones->login($_POST['email'],$_POST['Contraseña']);
-				}
-				
-			}
-		}
-	}
+	include '../controlers/iniciar.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,21 +9,21 @@ require_once '../administracion/validacion.php';
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 	<script src="https://kit.fontawesome.com/f0417ae4ab.js" crossorigin="anonymous"></script>
 	<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="img/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="img/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="../img/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="../img/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="../img/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="../img/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="../img/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="../img/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="../img/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="../img/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="../img/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="../img/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="../img/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../img/favicon-16x16.png">
 	<link rel="manifest" href="/manifest.json">
 	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="img/ms-icon-144x144.png">
+	<meta name="msapplication-TileImage" content="../img/ms-icon-144x144.png">
 	<meta name="theme-color" content="#ffffff">
 	<title>Iliberis</title>
 </head>
