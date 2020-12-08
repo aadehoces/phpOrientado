04dcecd45dbeda6aecd42f8f25ac180aa769bd34
raulcg99 <59688527+raulcg99@users.noreply.github.com>
2020-12-08 17:47:04 +0100

    Singleton

--- database/conexion.php
+++ database/conexion.php
@@ -1,8 +1,8 @@
 <?php 
 
-// Singleton to connect db.
+//Usamos Singleton para conectarnos a la base de datos
 class ConnectDb {
-   // Hold the class instance.
+   //La instancia para el Singleton
    private static $instance = null;
    private $conn;
    
@@ -11,14 +11,15 @@ class ConnectDb {
    private $pass = 'Aa123456_';
    private $name = 'pizzeria';
     
-   // The db connection is established in the private constructor.
+   //La conexion a la BD se crea en el constructor
    private function __construct()
    {
      $this->conn = new PDO("mysql:host={$this->host};
      dbname={$this->name}", $this->user,$this->pass,
      array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
    }
-   
+
+   //Llamamos a la instancia
    public static function getInstance()
    {
      if(!self::$instance)
@@ -29,6 +30,7 @@ class ConnectDb {
      return self::$instance;
    }
    
+   //Llamamos a la conexiona la DB
    public function getConnection()
    {
      return $this->conn;
